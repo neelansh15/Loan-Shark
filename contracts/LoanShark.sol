2721a61c7dac80ef8d31ec9bb5940899301282cc
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-07-02 21:13:48 +0530

    ft: LoanShark Contract

--- contracts/LoanShark.sol
+++ contracts/LoanShark.sol
@@ -3,20 +3,68 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
+/**
+    Supply ether as collateral to receive stablecoin as loan for a fee and pay back anytime.
+ */
 contract LoanShark is Ownable {
-    uint256 public fee;
     address public stablecoin;
+    IERC20 private token;
 
+    uint256 public fee;
+
+    // Ratio of Stablecoin-ETH. If Ratio = 2 => Get 2 * x Stablecoins for x Ether
+    uint8 public ratio;
+
+    event SetFee(uint256 oldFee, uint256 newFee, address indexed owner);
+    event SetStablecoin(
+        address oldStablecoin,
+        address newStablecoin,
+        address indexed owner
+    );
+    event Borrow(address indexed borrower, uint256 amount, uint256 timestamp);
+    event Repay(address indexed borrower, uint256 amount, uint256 timestamp);
+
+    constructor(address _stablecoin, uint8 _ratio) {
+        stablecoin = _stablecoin;
+        token = IERC20(token);
+        ratio = _ratio;
+    }
+
+    // Owner specific functions
     function withdraw() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
 
     function setFee(uint256 _fee) external onlyOwner {
+        emit SetFee(fee, _fee, msg.sender);
         fee = _fee;
     }
 
-    function borrow(uint256 _amount) external {
+    function setStablecoin(address _stablecoin) external onlyOwner {
+        emit SetStablecoin(stablecoin, _stablecoin, msg.sender);
+        stablecoin = _stablecoin;
+        token = IERC20(_stablecoin);
+    }
 
+    // Main functions
+    function borrow() external payable {
+        uint256 amount = msg.value;
+        uint256 balance = token.balanceOf(address(this));
+        require(amount > 0 && amount < balance, "Cannot process amount");
+
+        token.transfer(msg.sender, amount * ratio);
+
+        emit Borrow(msg.sender, amount, block.timestamp);
+    }
+
+    function repay(uint256 _amount) external {
+        require(token.balanceOf(msg.sender) >= _amount, "Insufficient Balance");
+
+        token.transferFrom(msg.sender, address(this), _amount);
+        payable(msg.sender).transfer(_amount / ratio);
+
+        emit Repay(msg.sender, _amount, block.timestamp);
     }
 }
