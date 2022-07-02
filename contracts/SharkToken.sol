762b3de295c8e24075f61cb01921a53791aa307b
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-07-02 21:14:42 +0530

    chore: change stablecoin token name and symbol

--- contracts/SharkToken.sol
+++ contracts/SharkToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SharkToken is ERC20 {
-    constructor() ERC20("SharkToken", "SHARK") {
+    constructor() ERC20("SharkUSDC", "sUSDC") {
         _mint(msg.sender, 100000 * 10**18);
     }
 }
