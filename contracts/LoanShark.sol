88c82b8ce79bfc2b3582b67b5a439baf1c4fc486
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-07-02 21:23:07 +0530

    ft: loan fees

--- contracts/LoanShark.sol
+++ contracts/LoanShark.sol
@@ -12,7 +12,9 @@ contract LoanShark is Ownable {
     address public stablecoin;
     IERC20 private token;
 
+    // Fees in Ether
     uint256 public fee;
+    uint256 public collectedFees;
 
     // Ratio of Stablecoin-ETH. If Ratio = 2 => Get 2 * x Stablecoins for x Ether
     uint8 public ratio;
@@ -26,15 +28,23 @@ contract LoanShark is Ownable {
     event Borrow(address indexed borrower, uint256 amount, uint256 timestamp);
     event Repay(address indexed borrower, uint256 amount, uint256 timestamp);
 
-    constructor(address _stablecoin, uint8 _ratio) {
+    constructor(
+        address _stablecoin,
+        uint8 _ratio,
+        uint256 _fee
+    ) {
         stablecoin = _stablecoin;
         token = IERC20(token);
         ratio = _ratio;
+        fee = _fee;
     }
 
     // Owner specific functions
     function withdraw() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
+        uint256 amount = collectedFees;
+        collectedFees = 0;
+
+        payable(owner()).transfer(amount);
     }
 
     function setFee(uint256 _fee) external onlyOwner {
@@ -49,6 +59,10 @@ contract LoanShark is Ownable {
     }
 
     // Main functions
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
     function borrow() external payable {
         uint256 amount = msg.value;
         uint256 balance = token.balanceOf(address(this));
@@ -62,8 +76,10 @@ contract LoanShark is Ownable {
     function repay(uint256 _amount) external {
         require(token.balanceOf(msg.sender) >= _amount, "Insufficient Balance");
 
+        collectedFees += fee;
+
         token.transferFrom(msg.sender, address(this), _amount);
-        payable(msg.sender).transfer(_amount / ratio);
+        payable(msg.sender).transfer((_amount / ratio) - fee);
 
         emit Repay(msg.sender, _amount, block.timestamp);
     }
